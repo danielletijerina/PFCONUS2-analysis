77755501bcb1411d463bd0e11b2c8ed3831c4066
Danielle Tijerina <dtijerina@princeton.edu>
2022-11-01 12:50:45 -0600

    add Casper run script

--- scripts/PFCLM_Averaging_Scripts/run_PF_avg.sh
+++ scripts/PFCLM_Averaging_Scripts/run_PF_avg.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#PBS -N oct_PF_avg
+#PBS -N oct_PF_avg_Casper
 #PBS -A UCSM0009
-#PBS -q regular 
+#PBS -q casper 
 #PBS -m bae
 #PBS -M dtijerina@princeton.edu
 #PBS -l walltime=12:00:00 
-#PBS -l select=1:mpiprocs=36
+#PBS -l select=1:ncpus=1:mem=100GB
 #PBS -j oe
 
-#PBS -o log.oe
+#PBS -o log_script.oe
 
 source /etc/profile.d/modules.sh
 ml conda
